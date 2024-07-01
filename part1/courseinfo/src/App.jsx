a5530e91d70e147ee5c61c1d1192ede5a4c718be
Federico LucidDream <federico.sabatini01@gmail.com>
2024-07-01 15:11:35 +0800

    Exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,9 +10,7 @@ const App = () => {
     return (
         <div>
             <Header course={course} />
-            <Content part={part1} exercises={exercises1} />
-            <Content part={part2} exercises={exercises2} />
-            <Content part={part3} exercises={exercises3} />
+            <Content part1={part1} part2={part2} part3={part3} exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
             <Total total={exercises1 + exercises2 + exercises3} />
         </div>
     )
@@ -26,11 +24,18 @@ const Header = (props) => {
 
 const Content = (props) => {
     return (
-        <p>
-            {props.part} {props.exercises}
-        </p>
+        <div>
+            <Part part={props.part1} exercises={props.exercises1} />
+            <Part part={props.part2} exercises={props.exercises2} />
+            <Part part={props.part3} exercises={props.exercises3} />
+        </div>
     )
+}
 
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
 }
 
 const Total = (props) => {
